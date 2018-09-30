8f4a3c33b2e55120194bcda154b406e2c2d2f01d
Keefer Taylor <keefer.t@gmail.com>
2018-09-30 23:18:12 +0200

    Add an abstract super class to response adapters.
    This allows the code to play nicely with generics in other classes and protocols (Specifically, this supports a RPC object that is coming in a future commit).

---
+++ Sources/Requests.swift
@@ -0,0 +1,9 @@
+//
+//  Requests.swift
+//  TezosKit
+//
+//  Created by Keefer Taylor on 9/30/18.
+//  Copyright © 2018 Keefer Taylor. All rights reserved.
+//
+
+import Foundation
--- Sources/ResponseAdapters.swift
+++ Sources/ResponseAdapters.swift
@@ -6,14 +6,21 @@ protocol ResponseAdapter {
   static func parse(input: Data) -> ParsedType?
 }
 
-public class StringResponseAdapter : ResponseAdapter {
-  public class func parse(input: Data) -> String? {
+/** Abstract class allows generics and associated types to play nicely together. */
+public class AbstractResponseAdapter<T>: ResponseAdapter {
+  public class func parse(input: Data) -> T? {
+    return nil
+  }
+}
+
+public class StringResponseAdapter : AbstractResponseAdapter<String> {
+  public override class func parse(input: Data) -> String? {
     return String(data: input, encoding: .ascii)
   }
 }
 
-public class JSONResponseAdapter : ResponseAdapter {
-  public class func parse(input: Data) -> Dictionary<String, Any>? {
+public class JSONResponseAdapter : AbstractResponseAdapter<[String : Any]> {
+  public override class func parse(input: Data) -> [String : Any]? {
     do {
       let json = try JSONSerialization.jsonObject(with: input)
       guard let typedJSON = json as? Dictionary<String, Any> else {
