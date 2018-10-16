1a22a3954ac73dac3b12d5aa2e5eb10b9df8dbaa
Keefer Taylor <keefer.t@gmail.com>
2018-10-16 21:45:32 +0100

    Hook up Seed -> Keypair cryptography. Mnemonic support to occur in a future commit.

--- Sources/KeyPair.swift
+++ Sources/KeyPair.swift
@@ -1,14 +1,22 @@
 import Foundation
+import Sodium
 
+/** Generic KeyPair protocol containing private and public keys. */
+// TODO: Refactor 'privateKey' to 'secretKey' to play nicely with Sodium's model object.
 public protocol KeyPair {
   var publicKey: [UInt8] { get }
   var privateKey: [UInt8] { get }
 }
 
+/** Generic KeyPair struct. */
 public struct DefaultKeyPair: KeyPair {
   public let publicKey: [UInt8]
   public let privateKey: [UInt8]
 }
 
-// TODO: Extend Sodium's Keypair behind this protocol when I can cajole Sodium into signing
-//       correctly
+/** Extension on Sodium's Sign.KeyPair to work with TezosKit code. */
+extension Sign.KeyPair: KeyPair {
+  public var privateKey: [UInt8] {
+    return secretKey
+  }
+}
--- Sources/Wallet.swift
+++ Sources/Wallet.swift
@@ -1,13 +1,31 @@
 import Foundation
+import Sodium
 
 public struct Wallet {
   public let publicKey: String
   public let privateKey: String
   public let address: String
 
-  public init(keyPair: KeyPair) {
+  /** Create a new wallet. */
+  public init?() {
+    // TODO: Generate seed string from mnenomomonic.
+    let seedString = "cce78b57ed8f4ec6767ed35f3aa41df525a03455e24bcc45a8518f63fbeda772"
+
+    let sodium = Sodium()
+    guard let seed = sodium.utils.hex2bin(seedString),
+          let keyPair = sodium.sign.keyPair(seed: seed) else {
+          return nil
+    }
+
+    self = Wallet(keyPair: keyPair)
+  }
+
+  /** Create a new wallet with the given keypair. */
+  private init(keyPair: KeyPair) {
     self.publicKey = Crypto.tezosPublicKey(from: keyPair.publicKey)
     self.privateKey = Crypto.tezosPrivateKey(from: keyPair.privateKey);
     self.address = Crypto.tezosPublicKeyHash(from: keyPair.publicKey);
   }
+
+  // TODO: Add support for passphrase generation.
 }
