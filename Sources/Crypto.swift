e325312d63dd491adef28fb2d313f65dfffa795a
Keefer Taylor <keefer.t@gmail.com>
2018-10-16 01:53:59 +0100

    Documenting crypto class to make it easier to work with in the future.

--- Sources/Crypto.swift
+++ Sources/Crypto.swift
@@ -1,12 +1,22 @@
 import Foundation
 import CommonCrypto
 
+/**
+ * A static helper class that provides utility functions for cyptography.
+ */
 public class Crypto {
   private static let publicKeyPrefix: [UInt8] = [13, 15, 37, 217] // edpk
   private static let privateKeyPrefix: [UInt8] = [43, 246, 78, 7] // edsk
 
+  /** Please do not instantiate this static helper class. */
+  private init() {}
 
-
+  /**
+   * Encode a Base58 address from the given key and prefix.
+   *
+   * The returned address is a Base58 encoded String with the following format:
+   *    [prefix][key][4 byte checksum]
+   */
   private static func encode(key: [UInt8], prefix: [UInt8]) -> String {
     let prefixedKey = prefix + key
     let prefixedKeyCheckSum = calculateCheckSum(prefixedKey)
@@ -15,12 +25,16 @@ public class Crypto {
     return String(base58Encoding: data)
   }
 
+  /**
+   * Calculate a checksum for a given input by hashing twice and then taking the first four bytes.
+   */
   private static func calculateCheckSum(_ input: [UInt8]) -> [UInt8] {
     let doubleHashedData = Data(input).sha256().sha256()
     let doubleHashedArray = Array(doubleHashedData)
     return Array(doubleHashedArray.prefix(4))
   }
 
+  /** Create a sha256 hash of the given data. */
   private func sha256(_ data: Data) -> Data? {
     guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else {
       return nil
@@ -30,4 +44,7 @@ public class Crypto {
               res.mutableBytes.assumingMemoryBound(to: UInt8.self))
     return res as Data
   }
+
+  /** Please do not instantiate this static helper class. */
+  private init() {}
 }
