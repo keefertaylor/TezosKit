3bc8edd013e3eb06111bde3ceb8b98c3978ffddc
Keefer Taylor <keefer.t@gmail.com>
2018-10-18 02:01:40 +0100

    Add utility method to return data needed to forge an operation for an address.

--- Sources/TezosClient.swift
+++ Sources/TezosClient.swift
@@ -47,6 +47,17 @@ public class TezosClient {
     self.sendRequest(rpc: rpc)
   }
 
+  public func setDelegate(for address: String, to delegateAddress: String, completion: @escaping (Error?) -> Void) {
+    guard let forgeData = getDataForSignedOperation(address: address) else {
+      let error = NSError(domain: tezosClientErrorDomain, code:TezosClientErrorCode.unknown.rawValue, userInfo: nil)
+      completion(error)
+      return
+    }
+
+    // TODO: Implement forge operation.
+    completion(nil)
+  }
+
   public func sendRequest<T>(rpc: TezosRPC<T>) {
     guard let remoteNodeEndpoint = URL(string: rpc.endpoint, relativeTo: self.remoteNodeURL) else {
       let error = NSError(domain: tezosClientErrorDomain, code:TezosClientErrorCode.unknown.rawValue, userInfo: nil)
@@ -58,5 +69,46 @@ public class TezosClient {
       rpc.handleResponse(data: data, error: error)
     }
     request.resume()
+  }
+
+  private func getDataForSignedOperation(address: String) -> (chainID: String, headHash: String, operationCounter: String)? {
+    let fetchersGroup = DispatchGroup()
+
+    var chainID: String? = nil
+    var headHash: String? = nil
+    let chainHeadRequestRPC = GetChainHeadRPC() { (json: [String : Any]?, error: Error?) in
+      if let json = json,
+         let returnedChainID = json["chain_id"] as? String,
+         let returnedHeadHash = json["hash"] as? String  {
+         chainID = returnedChainID
+         headHash = returnedHeadHash
+      }
+      fetchersGroup.leave()
+    }
+
+    var operationCounter: String? = nil
+    let getAddressCounterRPC =
+        GetAddressCounterRPC(address: address) { (returnedOperationCounter: String?, error: Error?) in
+      if let returnedOperationCounter = returnedOperationCounter {
+        operationCounter = returnedOperationCounter
+      }
+      fetchersGroup.leave()
+    }
+
+    fetchersGroup.enter()
+    self.sendRequest(rpc: chainHeadRequestRPC)
+
+    fetchersGroup.enter()
+    self.sendRequest(rpc: getAddressCounterRPC)
+
+    fetchersGroup.wait()
+
+    // If all data was retrived successfully return them in a tuple. Otherwise, return nil.
+    if let operationCounter = operationCounter,
+       let headHash = headHash,
+       let chainID = chainID {
+       return (chainID: chainID, headHash: headHash, operationCounter: operationCounter)
+    }
+    return nil;
   }
 }
