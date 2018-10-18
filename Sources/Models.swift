2b9c73fd5b538f13ae30126cbf4aeccaec758517
Keefer Taylor <keefer.t@gmail.com>
2018-10-18 01:51:24 +0100

    Fix crash when formatting balances with less than six significant digits

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -3,12 +3,20 @@ import Foundation
 public struct TezosBalance {
   public let balance: String
   public var formattedBalance: String {
-    let digitCount = balance.count
     let decimalDigitCount = 6
-    let integerDigitEndIndex = balance.index(balance.startIndex, offsetBy: digitCount - decimalDigitCount)
 
-    let integerString = balance[balance.startIndex..<integerDigitEndIndex]
-    let decimalString = balance[integerDigitEndIndex..<balance.endIndex]
+    // Pad small numbers with up to six zeros so that the below slicing works correctly
+    var paddedBalance = balance
+    while paddedBalance.count < decimalDigitCount {
+      paddedBalance = "0" + paddedBalance
+    }
+
+    let integerDigitEndIndex =
+        paddedBalance.index(paddedBalance.startIndex, offsetBy: paddedBalance.count - decimalDigitCount)
+
+    let integerString = paddedBalance[paddedBalance.startIndex..<integerDigitEndIndex].count > 0 ?
+        paddedBalance[paddedBalance.startIndex..<integerDigitEndIndex] : "0"
+    let decimalString = paddedBalance[integerDigitEndIndex..<paddedBalance.endIndex]
 
     return integerString + "." + decimalString
   }
