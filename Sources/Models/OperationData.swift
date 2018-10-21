f65aa2f0d3af1b37a4283bbf2cebabc613b5faca
Keefer Taylor <keefer.t@gmail.com>
2018-10-21 18:44:56 +0100

    Add an operation data object.

---
+++ Sources/Models/OperationData.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/**
+ * A lightweight property bag containing data about the current chain state in order to
+ * forge / preapply / inject operations on the Tezos blockchain.
+ */
+public struct OperationData {
+  /** The chain which is being operated on. */
+  public let chainID: String
+
+  /** The hash of the head of the chain being operated on. */
+  public let headHash: String
+
+  /** The hash of the protocol being operated on. */
+  public let protocolHash: String
+
+  /** The counter for the address being operated on. */
+  public let addressCounter: Int
+}
