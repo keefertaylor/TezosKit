e5799f0a97248a5573dd718650ba1f2376f02259
Keefer Taylor <keefer.t@gmail.com>
2018-10-21 14:32:02 +0100

    Decompose ResponseAdapters.swift to individual classes. Add documentation along the way.

---
+++ Sources/RPC/ResponseAdapters/AbstractResponseAdapter.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/**
+ * An abstract super class for all response adapters. This class is a shim to allow generics and
+ * associated types to play nicely together.
+ */
+public class AbstractResponseAdapter<T>: ResponseAdapter {
+  public class func parse(input: Data) -> T? {
+    fatalError("Use a concrete implementation of the response adapter class")
+    return nil
+  }
+
+  /** Please do not instantiate adapters. Adapters should only be used as static utility classes. */
+  private init() {}
+}
