73e5f7a7ad976ed94082591e39d1742f3bb3a287
Keefer Taylor <keefer.t@gmail.com>
2018-10-22 00:07:03 +0100

    Add RevealOperation

---
+++ Sources/Operation/RevealOperation.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+/**
+ * An operation to reveal an address.
+ *
+ * Note that TezosKit will automatically inject this operation when required for supported
+ * operations.
+ */
+public class RevealOperation: AbstractOperation {
+  public override var dictionaryRepresentation: [String: String] {
+    var operation = super.dictionaryRepresentation
+    operation["public_key"] = self.publicKey
+    return operation
+  }
+
+  /** The public key for the address being revealed. */
+  public let publicKey: String
+
+  /**
+   * Initialize a new reveal operation.
+   *
+   * @param address The address to reveal.
+   * @param publicKey The public key of the address to reveal.
+   */
+  public init(from address: String, publicKey: String) {
+    self.publicKey = publicKey
+    super.init(source: address,
+               kind: .reveal,
+               fee: AbstractOperation.zeroTezosBalance,
+               gasLimit:  AbstractOperation.zeroTezosBalance,
+               storageLimit: AbstractOperation.zeroTezosBalance)
+  }
+}
+
