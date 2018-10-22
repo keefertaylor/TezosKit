89b79eef09b1d2e81e9368dc75e40ce062dc82c9
Keefer Taylor <keefer.t@gmail.com>
2018-10-22 03:16:28 +0100

    Wire up DelegationOperation and document the class.

--- Sources/Operation/DelegationOperation.swift
+++ Sources/Operation/DelegationOperation.swift
@@ -1,22 +1,28 @@
 import Foundation
 
-/**
- * An operation to set a delegate for an address.
- *
- * @warning This class is not functional... yet. 
- */
+/** An operation to set a delegate for an address. */
 public class DelegationOperation: AbstractOperation {
+  /** The address that will be set as the delegate. */
 	public let delegate: String
 
 	public override var dictionaryRepresentation: [String: String] {
-		// TODO: Implement.
-		return super.dictionaryRepresentation
+		var operation = super.dictionaryRepresentation
+    operation["delegate"] = delegate
+    return operation
 	}
 
+  /**
+   * @param wallet The wallet that will delegate funds.
+   * @param delegate The address to delegate to.
+   */
 	public convenience init(from wallet: Wallet, to delegate: String) {
 		self.init(source: wallet.address, to: delegate)
 	}
 
+  /**
+   * @param source The address that will delegate funds.
+   * @param delegate The address to delegate to.
+   */
 	public init(source: String, to delegate: String) {
 		self.delegate = delegate
 		super.init(source: source, kind: .delegation)
