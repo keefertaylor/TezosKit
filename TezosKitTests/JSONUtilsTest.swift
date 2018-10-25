fe5c5d38035066a5d91f5f527583cd96160ea627
Keefer Taylor <keefer.t@gmail.com>
2018-10-25 20:52:53 +0100

    Fix test.

--- TezosKitTests/JSONUtilsTest.swift
+++ TezosKitTests/JSONUtilsTest.swift
@@ -3,6 +3,6 @@ import XCTest
 class JSONUtilsTest: XCTestCase {
     func testExample() {
       // Purposefully assert false to prove that we can break CI with bad tests.
-      XCTAssert(false)
+      XCTAssert(true)
     }
 }
