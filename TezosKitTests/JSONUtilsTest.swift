e82d653e6e4e95a6ac24b44ada250df1343768eb
Keefer Taylor <keefer.t@gmail.com>
2018-10-25 20:15:26 +0100

    Add a failing test. Want to make sure CI tests fail.

---
+++ TezosKitTests/JSONUtilsTest.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+class JSONUtilsTest: XCTestCase {
+    func testExample() {
+      // Purposefully assert false to prove that we can break CI with bad tests.
+      XCTAssert(false)
+    }
+}
