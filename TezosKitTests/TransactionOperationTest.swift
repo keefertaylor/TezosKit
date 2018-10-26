7532020dd30fb4c655172018bb0f2ab5b8a3f5ab
Keefer Taylor <keefer.t@gmail.com>
2018-10-26 18:29:07 +0100

    Add TransactionOperationTest.

---
+++ TezosKitTests/TransactionOperationTest.swift
@@ -0,0 +1,44 @@
+import XCTest
+import TezosKit
+
+class TransactionOperationTest: XCTestCase {
+  let destination = "tz1def"
+  let balance = TezosBalance(balance: 3.50)
+
+  public func testTransation() {
+    let source = "tz1abc"
+
+    let operation = TransactionOperation(amount: balance, source: source, destination: destination)
+    let dictionary = operation.dictionaryRepresentation
+
+    XCTAssertNotNil(dictionary["source"])
+    XCTAssertEqual(dictionary["source"], source)
+
+    XCTAssertNotNil(dictionary["destination"])
+    XCTAssertEqual(dictionary["destination"], destination)
+
+    XCTAssertNotNil(dictionary["amount"])
+    XCTAssertEqual(dictionary["amount"], balance.rpcRepresentation)
+  }
+
+  public func testTransationFromWallet() {
+    guard let wallet = Wallet() else {
+      XCTFail()
+      return
+    }
+
+    let operation = TransactionOperation(amount: balance, source: wallet, destination: destination)
+    let dictionary = operation.dictionaryRepresentation
+
+    XCTAssertNotNil(dictionary["source"])
+    XCTAssertEqual(dictionary["source"], wallet.address)
+
+    XCTAssertNotNil(dictionary["destination"])
+    XCTAssertEqual(dictionary["destination"], destination)
+
+    XCTAssertNotNil(dictionary["amount"])
+    XCTAssertEqual(dictionary["amount"], balance.rpcRepresentation)
+  }
+
+
+}
