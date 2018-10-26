eeab544bcf697d2948660f94e5f8ea5a5de0b348
Keefer Taylor <keefer.t@gmail.com>
2018-10-26 19:08:00 +0100

    Add IntegerResponseAdapterTests

---
+++ TezosKitTests/IntegerResponseAdapterTest.swift
@@ -0,0 +1,56 @@
+import XCTest
+import TezosKit
+
+class IntegerResponseAdapterTest: XCTestCase {
+  let integer = 123
+  let integerString = "123"
+
+
+  public func testParseInteger() {
+    guard let balanceData = integerString.data(using: .utf8),
+          let parsedInteger = IntegerResponseAdapter.parse(input: balanceData) else {
+      XCTFail()
+      return
+    }
+
+    XCTAssertNotNil(parsedInteger)
+    XCTAssertEqual(parsedInteger, integer)
+  }
+
+  // Balances are returned as quoted from the API. Make sure that quotes can be stripped when
+  // parsing.
+  public func testParseIntegerWithQuotes() {
+    guard let balanceData = ("\"" + integerString + "\"").data(using: .utf8),
+          let parsedInteger = IntegerResponseAdapter.parse(input: balanceData) else {
+      XCTFail()
+      return
+    }
+
+    XCTAssertNotNil(parsedInteger)
+    XCTAssertEqual(parsedInteger, integer)
+  }
+
+  // Ensure white space does not mess up parsing.
+  public func testParseIntegerWithWhitespace() {
+    guard let balanceData = ("    " + integerString + "\n\n\n").data(using: .utf8),
+          let parsedInteger = IntegerResponseAdapter.parse(input: balanceData) else {
+      XCTFail()
+      return
+    }
+
+    XCTAssertNotNil(parsedInteger)
+    XCTAssertEqual(parsedInteger, integer)
+  }
+
+  // Ensure invalid strings cannot be parsed.
+  public func testParseIntegerWithInvalidInput() {
+    let invalidIntegerString = "xyz"
+    guard let invalidIntegerData = invalidIntegerString.data(using: .utf8) else {
+      XCTFail()
+      return
+    }
+    let parsedInteger = IntegerResponseAdapter.parse(input: invalidIntegerData)
+
+    XCTAssertNil(parsedInteger)
+  }
+}
