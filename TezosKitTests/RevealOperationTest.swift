bb50c30e70ed6a220b8d0f6d5f59374901602c4c
Keefer Taylor <keefer.t@gmail.com>
2018-10-26 18:21:13 +0100

    Add Reveal Operation Test.

---
+++ TezosKitTests/RevealOperationTest.swift
@@ -0,0 +1,35 @@
+import XCTest
+import TezosKit
+
+class RevealOperationTest: XCTestCase {
+  public func testDictionaryRepresentation() {
+    let source = "tz1abc"
+    let publicKey = "edpkXYZ"
+
+    let operation = RevealOperation(from: source, publicKey: publicKey)
+    let dictionary = operation.dictionaryRepresentation
+
+    XCTAssertNotNil(dictionary["source"])
+    XCTAssertEqual(dictionary["source"], source)
+
+    XCTAssertNotNil(dictionary["public_key"])
+    XCTAssertEqual(dictionary["public_key"], publicKey)
+  }
+
+  public func testDictionaryRepresentationFromWallet() {
+    guard let wallet = Wallet() else {
+      XCTFail()
+      return
+    }
+
+    let operation = RevealOperation(from: wallet)
+    let dictionary = operation.dictionaryRepresentation
+
+    XCTAssertNotNil(dictionary["source"])
+    XCTAssertEqual(dictionary["source"], wallet.address)
+
+    XCTAssertNotNil(dictionary["public_key"])
+    XCTAssertEqual(dictionary["public_key"], wallet.keys.publicKey)
+  }
+
+}
