568d48e3384f055916839b0fb96eb0b44970bf8a
keefertaylor <keefer.t@gmail.com>
2018-11-05 20:12:26 +0000

    Add equality for keys. Add associated unit tests.

---
+++ TezosKitTests/KeysTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import TezosKit
+
+class KeysTests: XCTestCase {
+  public func testEquality() {
+    let keys1 = Keys(publicKey: "a", secretKey: "a")
+    let keys2 = Keys(publicKey: "a", secretKey: "a")
+    XCTAssertEqual(keys1, keys2)
+
+    let keys3 = Keys(publicKey: "a", secretKey: "b")
+    let keys4 = Keys(publicKey: "b", secretKey: "a")
+    XCTAssertNotEqual(keys1, keys3)
+    XCTAssertNotEqual(keys1, keys4)
+  }
+}
