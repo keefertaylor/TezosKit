d2697e65e32257d8dbd2260aef22400f4daddefc
keefertaylor <keefer.t@gmail.com>
2018-11-27 18:41:22 +0000

    Fixing types on get ballot list

--- Sources/RPC/GetBallotsListRPC.swift
+++ Sources/RPC/GetBallotsListRPC.swift
@@ -5,15 +5,14 @@ import Foundation
 /**
  * An RPC which will retrieve ballots cast so far during a voting period.
  */
-public class GetBallotsListRPC: TezosRPC<String> {
+public class GetBallotsListRPC: TezosRPC<[[String: Any]]> {
   /**
-   * @param blockID The level to count ballots at.
    * @param completion A block to be called at the completion of the operation.
    */
-  public init(blockID: UInt, completion: @escaping (String?, Error?) -> Void) {
-    let endpoint = "chains/main/blocks/\(blockID)/votes/ballot_list"
+  public init(completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+    let endpoint = "chains/main/blocks/head/votes/ballot_list"
     super.init(endpoint: endpoint,
-               responseAdapterClass: StringResponseAdapter.self,
+               responseAdapterClass: JSONArrayResponseAdapter.self,
                completion: completion)
   }
 }
--- Sources/RPC/GetProposalsListRPC.swift
+++ Sources/RPC/GetProposalsListRPC.swift
@@ -5,14 +5,14 @@ import Foundation
 /**
  * An RPC which will retrieve a list of proposals with number of supporters.
  */
-public class GetProposalsListRPC: TezosRPC<String> {
+public class GetProposalsListRPC: TezosRPC<[[String: Any]]> {
   /**
    * @param completion A block to be called at the completion of the operation.
    */
-  public init(completion: @escaping (String?, Error?) -> Void) {
+  public init(completion: @escaping ([[String: Any]]?, Error?) -> Void) {
     let endpoint = "chains/main/blocks/head/votes/proposals"
     super.init(endpoint: endpoint,
-               responseAdapterClass: StringResponseAdapter.self,
+               responseAdapterClass: JSONArrayResponseAdapter.self,
                completion: completion)
   }
 }
