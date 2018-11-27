81f653270cfd6d36ad19b2ccb0e1a2d6d4ea2fb1
keefertaylor <keefer.t@gmail.com>
2018-11-27 18:31:22 +0000

    Fix types on quorum RPC

--- Sources/RPC/GetExpectedQuorumRPC.swift
+++ Sources/RPC/GetExpectedQuorumRPC.swift
@@ -5,15 +5,14 @@ import Foundation
 /**
  * An RPC which will retrieve the expected quorum.
  */
-public class GetExpectedQuorumRPC: TezosRPC<String> {
+public class GetExpectedQuorumRPC: TezosRPC<Int> {
   /**
-   * @param blockID The level to examine the quorum at.
    * @param completion A block to be called at the completion of the operation.
    */
-  public init(blockID: UInt, completion: @escaping (String?, Error?) -> Void) {
-    let endpoint = "chains/main/blocks/\(blockID)/votes/current_quorum"
+  public init(completion: @escaping (Int?, Error?) -> Void) {
+    let endpoint = "chains/main/blocks/head/votes/current_quorum"
     super.init(endpoint: endpoint,
-               responseAdapterClass: StringResponseAdapter.self,
+               responseAdapterClass: IntegerResponseAdapter.self,
                completion: completion)
   }
 }
--- TezosKitTests/GetExpectedQuorumRPCTest.swift
+++ TezosKitTests/GetExpectedQuorumRPCTest.swift
@@ -5,9 +5,9 @@ import XCTest
 
 class GetExpectedQuorumRPCTest: XCTestCase {
   public func testGetExpectedQuorumRPCTest() {
-    let rpc = GetExpectedQuorumRPC(blockID: 1000) { _, _ in }
+    let rpc = GetExpectedQuorumRPC() { _, _ in }
 
-    XCTAssertEqual(rpc.endpoint, "chains/main/blocks/1000/votes/current_quorum")
+    XCTAssertEqual(rpc.endpoint, "chains/main/blocks/head/votes/current_quorum")
     XCTAssertNil(rpc.payload)
     XCTAssertFalse(rpc.isPOSTRequest)
   }
