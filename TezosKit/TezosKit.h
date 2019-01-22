c7e644e9260fc8dfbf00618583de2a6a22b90646
keefertaylor <keefer.t@gmail.com>
2019-01-22 23:29:38 +0000

    Start anew with a new xcodeproj

---
+++ TezosKit/TezosKit.h
@@ -0,0 +1,19 @@
+//
+//  TezosKit.h
+//  TezosKit
+//
+//  Created by Keefer Taylor on 1/22/19.
+//  Copyright © 2019 Keefer Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TezosKit.
+FOUNDATION_EXPORT double TezosKitVersionNumber;
+
+//! Project version string for TezosKit.
+FOUNDATION_EXPORT const unsigned char TezosKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TezosKit/PublicHeader.h>
+
+
