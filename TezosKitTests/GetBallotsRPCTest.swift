c5308797d3a7857b9d187cca7e958820bb84423c
keefertaylor <keefer.t@gmail.com>
2019-01-23 22:25:21 +0000

    Fix tests

--- TezosKitTests/GetBallotsRPCTest.swift
+++ TezosKitTests/GetBallotsRPCTest.swift
@@ -9,6 +9,6 @@ class GetBallotsRPCTest: XCTestCase {
 
     XCTAssertEqual(rpc.endpoint, "chains/main/blocks/head/votes/ballots")
     XCTAssertNil(rpc.payload)
-    XCTAssertTrue(rpc.isPOSTRequest)
+    XCTAssertFalse(rpc.isPOSTRequest)
   }
 }
