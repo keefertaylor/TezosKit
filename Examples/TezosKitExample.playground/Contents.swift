b7108a7b1f128b975d53358046fb2017b7f51ec7
keefertaylor <keefer.t@gmail.com>
2019-01-26 12:44:08 +0000

    run formatter

--- Examples/TezosKitExample.playground/Contents.swift
+++ Examples/TezosKitExample.playground/Contents.swift
@@ -1,3 +1,5 @@
+// Copyright Keefer Taylor, 2018
+
 import TezosKit
 
 let tezosClient = TezosClient()
@@ -6,23 +8,23 @@ let wallet = Wallet()!
 tezosClient.getHeadHash { headHash, error in
   guard let headHash = headHash,
     error == nil else {
-      print("Couldn't fetch head :(")
-      if let error = error {
-        print("Error: \(error)")
-      }
-      return
+    print("Couldn't fetch head :(")
+    if let error = error {
+      print("Error: \(error)")
+    }
+    return
   }
   print("The hash of the head block was \(headHash)")
 }
 
-tezosClient.getBalance(wallet: wallet) { (balance, error) in
+tezosClient.getBalance(wallet: wallet) { balance, error in
   guard let balance = balance,
     error == nil else {
-      print("Couldn't fetch balance :(")
-      if let error = error {
-        print("Error: \(error)")
-      }
-      return
+    print("Couldn't fetch balance :(")
+    if let error = error {
+      print("Error: \(error)")
+    }
+    return
   }
   print("The balance of \(wallet.address) was \(balance.humanReadableRepresentation)")
 }
