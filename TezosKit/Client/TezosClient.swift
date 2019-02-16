0f81af0b9c3bd3aa45341d884c906170aa37071a
keefertaylor <keefer.t@gmail.com>
2019-02-16 19:31:23 +0000

    Draft a response handler

--- TezosKit/Client/TezosClient.swift
+++ TezosKit/Client/TezosClient.swift
@@ -74,8 +74,8 @@ public class TezosClient {
   /** A URL pointing to a remote node that will handle requests made by this client. */
   private let remoteNodeURL: URL
 
-  /** The queue that callbacks from requests will be made on. */
-  private let callbackQueue: DispatchQueue
+  /** A response handler for RPCs. */
+  private let responseHandler: RPCResponseHandler
 
   /**
    * Initialize a new TezosClient.
@@ -91,7 +91,7 @@ public class TezosClient {
   ) {
     self.remoteNodeURL = remoteNodeURL
     self.urlSession = urlSession
-    self.callbackQueue = callbackQueue
+    self.responseHandler = RPCResponseHandler(callbackQueue)
   }
 
   /** Retrieve data about the chain head. */
@@ -563,37 +563,7 @@ public class TezosClient {
       guard let self = self else {
         return
       }
-
-      // Check if the response contained a 200 HTTP OK response. If not, then propagate an error.
-      if let httpResponse = response as? HTTPURLResponse,
-        httpResponse.statusCode != 200 {
-        // Default to unknown error and try to give a more specific error code if it can be narrowed
-        // down based on HTTP response code.
-        var errorKind: TezosClientError.ErrorKind = .unknown
-        // Status code 40X: Bad request was sent to server.
-        if httpResponse.statusCode >= 400, httpResponse.statusCode < 500 {
-          errorKind = .unexpectedRequestFormat
-          // Status code 50X: Bad request was sent to server.
-        } else if httpResponse.statusCode >= 500 {
-          errorKind = .unexpectedResponse
-        }
-
-        // Decode the server's response to a string in order to bundle it with the error if it is in
-        // a readable format.
-        var errorMessage = ""
-        if let data = data,
-          let dataString = String(data: data, encoding: .utf8) {
-          errorMessage = dataString
-        }
-
-        // Drop data and send our error to let subsequent handlers know something went wrong and to
-        // give up.
-        let error = TezosClientError(kind: errorKind, underlyingError: errorMessage)
-        rpc.handleResponse(data: nil, error: error, callbackQueue: self.callbackQueue)
-        return
-      }
-
-      rpc.handleResponse(data: data, error: error, callbackQueue: self.callbackQueue)
+      self.responseHandler.handleResponse()
     }
     request.resume()
   }
