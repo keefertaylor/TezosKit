827a6ba644f1a16bc41acc5754249b8828f8e592
keefertaylor <keefer.t@gmail.com>
2019-02-19 03:33:26 +0000

    Hookup Valet to Carthage.

--- TezosKit/Utils/WalletKeychainManager.swift
+++ TezosKit/Utils/WalletKeychainManager.swift
@@ -14,37 +14,37 @@ public class WalletKeychainManager {
   private let tezosKitWalletIdentifier = "TezosKitWalletIdentifier"
 
   /// Valet instance which wraps a secure keystore.
-  private let valet: Valet
+  private let valet: SecureEnclaveValet
 
   /** Initialize a new WalletKeychainManager. */
   public init() {
     let identifier = Identifier(nonEmpty: tezosKitValetIdentifier)!
-    let mySecureEnclaveValet = SecureEnclaveValet.valet(with: identifier, accessControl: .userPresence)
+    valet = SecureEnclaveValet.valet(with: identifier, accessControl: .userPresence)
   }
 
-  /**
-   * Tests if there is an existing wallet stored in the keychain.
-   * - Returns: True if there is an existing wallet.
-   */
-  public func hasWallet() -> Bool {
-  }
-
-  /**
-   * Store the given wallet in the keychain. This will overwrite any wallet in the keychain at this location.
-   * - Parameter wallet: The wallet to save in the keychain.
-   */
-  public func storeWallet(_ wallet: Wallet) {
-  }
-
-  /**
-   * Restore the given wallet in the keychain.
-   */
-  public func restoreWallet() -> Wallet? {
-  }
-
-  /**
-   * Clear the wallet from the keychain.
-   */
-  public func clear() {
-  }
+//  /**
+//   * Tests if there is an existing wallet stored in the keychain.
+//   * - Returns: True if there is an existing wallet.
+//   */
+//  public func hasWallet() -> Bool {
+//  }
+//
+//  /**
+//   * Store the given wallet in the keychain. This will overwrite any wallet in the keychain at this location.
+//   * - Parameter wallet: The wallet to save in the keychain.
+//   */
+//  public func storeWallet(_ wallet: Wallet) {
+//  }
+//
+//  /**
+//   * Restore the given wallet in the keychain.
+//   */
+//  public func restoreWallet() -> Wallet? {
+//  }
+//
+//  /**
+//   * Clear the wallet from the keychain.
+//   */
+//  public func clear() {
+//  }
 }
