1d00758e373a15c4cd6b149f8a292d6995abf75f
keefertaylor <keefer.t@gmail.com>
2019-03-01 01:47:11 +0000

    RunOperationRPC tests

--- TezosKit/RPC/RunOperationRPC.swift
+++ TezosKit/RPC/RunOperationRPC.swift
@@ -6,7 +6,7 @@ import Foundation
 public class RunOperationRPC: RPC<[String: Any]> {
   /// - Parameter signedOperationPayload: A payload containing an operation to run.
   public init(signedOperationPayload: SignedOperationPayload) {
-    let endpoint = "chains/main/blocks/head/helpers/scripts/run_operation"
+    let endpoint = "/chains/main/blocks/head/helpers/scripts/run_operation"
     let jsonPayload = JSONUtils.jsonString(for: signedOperationPayload.dictionaryRepresentation)
     super.init(
       endpoint: endpoint,
