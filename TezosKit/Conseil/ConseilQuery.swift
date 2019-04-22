a3b0056cbfe7fb2882f562b53875b691d3badaff
keefertaylor <keefer.t@gmail.com>
2019-04-22 13:38:26 +0100

    More conseil fixes

--- TezosKit/Conseil/ConseilQuery.swift
+++ TezosKit/Conseil/ConseilQuery.swift
@@ -14,24 +14,37 @@ public enum ConseilQuery: String {
     case field
     case operation
     public enum Operation: String {
+      case after = "after"
+      case before
+      case between
+      case endsWith
       case equal = "eq"
+      case greaterThan = "gt"
+      case `in` = "in"
+      case isNull = "isnull"
+      case lessThan = "lt"
+      case like
+      case startsWith
     }
     case inverse
 
     public static func predicateWith(
       field: String,
-      set: [String],
       operation: ConseilQuery.Predicates.Operation = .equal,
+      set: [String] = [],
       inverse: Bool = false
     ) -> ConseilPredicate {
-      return [
+      var predicate: [String: Any] = [
         ConseilQuery.Predicates.field.rawValue: field,
-        ConseilQuery.Predicates.set.rawValue: set,
         ConseilQuery.Predicates.operation.rawValue: operation.rawValue,
-        ConseilQuery.Predicates.inverse.rawValue: inverse
+        ConseilQuery.Predicates.inverse.rawValue: inverse,
+        ConseilQuery.Predicates.set.rawValue: set
       ]
+      return predicate
     }
   }
+
+  case aggregation
 
   case orderBy = "orderby"
   public enum OrderBy: String {
