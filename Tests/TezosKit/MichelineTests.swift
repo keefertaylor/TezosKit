ecfe12690809bde3e4ed249fb97a1c58273f9172
keefertaylor <keefer.t@gmail.com>
2019-07-27 00:30:45 -0700

    fix todo

--- Tests/TezosKit/MichelineTests.swift
+++ Tests/TezosKit/MichelineTests.swift
@@ -125,10 +125,14 @@ final class MichelineTests: XCTestCase {
     XCTAssertEqual(encoded, fixExpected(expected))
   }
 
-  // TODO: Cleanup this fn.
+  /// Fix the expected input to the expected output of Swift's JSON serializer.
+  ///
+  /// Expected output is taken from the human readable version of the JSON serialization format. Swift outputs JSON
+  /// keys either (1) non-deterministically or (2) ordered by key. This function re-orders the expected outputs of a
+  /// input JSON string by key so that asserts can work properly.
   func fixExpected(_ expected: String) -> String {
     let data = expected.data(using: .utf8)!
-    let dictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:AnyObject]
+    let dictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
     return JSONUtils.jsonString(for: dictionary)!
   }
 }
