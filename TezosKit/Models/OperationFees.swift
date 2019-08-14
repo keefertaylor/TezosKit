dc984b4a72085068d771352fead001f16deaa10f
keefertaylor <keefer.t@gmail.com>
2019-08-14 17:19:25 -0400

    Allow external instantiation of OperationFees

--- TezosKit/Models/OperationFees.swift
+++ TezosKit/Models/OperationFees.swift
@@ -7,4 +7,10 @@ public struct OperationFees {
   public let fee: Tez
   public let gasLimit: Int
   public let storageLimit: Int
+
+  public init(fee: Tez, gasLimit: Int, storageLimit: Int) {
+    self.fee = fee
+    self.gasLimit = gasLimit
+    self.storageLimit = storageLimit
+  }
 }
