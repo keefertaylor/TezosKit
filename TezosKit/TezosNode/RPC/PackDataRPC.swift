a3a66fe58006fcb9d907074c078c6281a5d2c97f
keefertaylor <keefer.t@gmail.com>
2020-01-09 00:09:20 -0500

    pack data RPC

---
+++ TezosKit/TezosNode/RPC/PackDataRPC.swift
@@ -0,0 +1,20 @@
+// Copyright Keefer Taylor, 2020
+
+import Foundation
+
+/// An RPC which will pack data to a binary format.
+public class PackDataRPC: RPC<String> {
+  public init(
+    input: [String: Any]
+  ) {
+    let endpoint = "/chains/main/blocks/head/helpers/scripts/pack_data"
+    let payload = JSONUtils.jsonString(for: input)
+
+    super.init(
+      endpoint: endpoint,
+      responseAdapterClass: StringResponseAdapter.self,
+      payload: payload
+    )
+  }
+}
+
