6877281434abe93606b019296130978ce3a3d729
keefertaylor <keefer.t@gmail.com>
2020-01-28 00:20:55 -0500

    more demo

--- Examples/SecureEnclave/ViewController.swift
+++ Examples/SecureEnclave/ViewController.swift
@@ -1,52 +1,33 @@
-//
-//  ViewController.swift
-//  Enclave
-//
-//  Created by Keefer Taylor on 5/1/19.
-//  Copyright © 2019 Keefer Taylor. All rights reserved.
-//
+// Copyright Keefer Taylor, 2020.
+
 import TezosKit
 import UIKit
 
-class BackgroundHighlightedButton: UIButton {
-  @IBInspectable var highlightedBackgroundColor: UIColor?
-  @IBInspectable var nonHighlightedBackgroundColor: UIColor?
-  override var isHighlighted: Bool {
-    get {
-      return super.isHighlighted
-    }
-    set {
-      if newValue {
-        self.backgroundColor = .blue
-      } else {
-        self.backgroundColor = .gray
-      }
-      super.isHighlighted = isHighlighted
-    }
-  }
-}
-
 class ViewController: UIViewController {
+  /// UI Components.
   public let titleLabel: UILabel = UILabel()
-
   public let generateKey: UIButton = BackgroundHighlightedButton()
   public let pkh: UILabel = UILabel()
   public let genKey: UILabel = UILabel()
   public let dest: UILabel = UILabel()
   public let destKey: UILabel = UILabel()
-
-  var nodeClient: TezosNodeClient?
-
   public let signTransaction: UIButton = BackgroundHighlightedButton()
   public let h = UILabel()
   public let hash2 = UILabel()
-
   public let open: UIButton = BackgroundHighlightedButton()
+
+  /// The wallet that we're interacting with.
+  var wallet: SignatureProvider?
+
+  /// The last operation hash received from TezosClient.
+  var opHash: String?
+
+  // MARK: - UIView
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-//    title.setTitle("Tezos Secure Enclave Signing")
+    titleLabel.text = "Tezos Secure Enclave Signing"
     generateKey.setTitle("Generate Key", for: .normal)
     generateKey.frame = CGRect(x: 50, y: 200, width: 300, height: 50)
     generateKey.backgroundColor = .gray
@@ -83,45 +64,51 @@ class ViewController: UIViewController {
     hash2.frame = CGRect(x: 50, y: 570, width: 300, height: 20)
     self.view.addSubview(hash2)
 
-    open.setTitle("Open in TzScan", for: .normal)
+    open.setTitle("Open in Block Explorer", for: .normal)
     open.frame = CGRect(x: 50, y: 610, width: 300, height: 50)
     open.backgroundColor = .gray
     open.addTarget(self, action: #selector(openFn), for: .touchUpInside)
     self.view.addSubview(open)
   }
 
+  // MARK: - User Interaction
+
   @objc
   func generate() {
-    // TODO: Update this
-    // TODO: REcall key compression.
-    genKey.text = "tz3fq2juip9PQfxnQ5JTdwzEscdkVpWi2rTs"
-  }
+    let wallet = Wallet()
 
-  var opHash: String?
+    genKey.text = wallet?.address
+
+    self.wallet = wallet
+  }
 
   @objc
   func send() {
-//    let nodeURL = URL(string: "http://alphanet-node.tzscan.io:80")! // URL(string: "http://127.0.0.1:8732")!
-//    nodeClient = TezosNodeClient(remoteNodeURL: nodeURL)
-//
-//    let wallet = Wallet()!
-//    nodeClient!.send(
-//    amount: Tez("1")!, to: "tz1NpWrAyDL9k2Lmnyxcgr9xuJakbBxdq7FB", from: "tz3fq2juip9PQfxnQ5JTdwzEscdkVpWi2rTs", keys: wallet) { result in
-//      switch result {
-//      case .failure(let error):
-//        print("WRONG")
-//        print(error)
-//      case .success(let hash):
-//        self.opHash = hash
-//        self.hash2.text = hash
-//      }
-//    }
+    guard let wallet = self.wallet else {
+      return
+    }
+
+    let nodeURL = URL(string: "https://tezos-dev.cryptonomic-infra.tech")!
+    let nodeClient = TezosNodeClient(remoteNodeURL: nodeURL)
+    nodeClient.send(
+      amount: Tez("1")!,
+      to: "tz1NpWrAyDL9k2Lmnyxcgr9xuJakbBxdq7FB",
+      from: "x",
+      signatureProvider: wallet,
+      operationFeePolicy: .estimate
+    ) { result in
+      switch result {
+      case .success(let opHash):
+        self.opHash = opHash
+      case .failure(let error):
+        print("error :( \(error)")
+      }
+    }
   }
 
   @objc
   func openFn() {
-    let url = URL(string: "https://alphanet.tzscan.io/" + opHash!)!
+    let url = URL(string: "https://babylonnet.tzstats.com/" + opHash!)!
     UIApplication.shared.open(url)
   }
-
 }
