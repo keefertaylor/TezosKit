36f51dba41b70f63c12136bed00a29893e5bfa8a
keefertaylor <keefer.t@gmail.com>
2020-02-16 15:08:50 -0500

    introduce a codable protocol

--- TezosKit/Utils/JSONUtils.swift
+++ TezosKit/Utils/JSONUtils.swift
@@ -2,6 +2,13 @@
 
 import Foundation
 
+/// Marks an object as codable to JSON.
+public protocol JSONCodable {}
+
+/// Mark arrays and dictionaries as JSONCodable.
+extension Dictionary: JSONCodable where Key == String {}
+extension Array: JSONCodable {}
+
 /// A facade for a JSON parsing library.
 public enum JSONUtils {
   /// Returns a JSON encoded string representation of a given string.
@@ -9,30 +16,13 @@ public enum JSONUtils {
     return "\"" + string + "\""
   }
 
-  /// Returns a JSON encoded string representation of a given array.
-  public static func jsonString(for array: [[String: Any]]) -> String? {
-    return jsonString(forUntypedObject: array)
-  }
-
-  /// Returns a JSON encoded string representation of a given dictionary.
-  public static func jsonString(for dictionary: [String: Any]) -> String? {
-    return jsonString(forUntypedObject: dictionary)
-  }
-
   /// Return a JSON encoded string representation of a given integer.
   public static func jsonString(for int: Int) -> String? {
     return jsonString(for: String(int))
   }
 
-  // TODO(keefertaylor): Convert this to be a protocl.
-  public static func jsonString(for object: Any) -> String? {
-    return jsonString(forUntypedObject: object)
-  }
-
-  /// Private untyped helper that returns a JSON encoded representation of the given object.
-  ///
-  /// - Note: The only supported objects are Arrays and Dictionaries.
-  private static func jsonString(forUntypedObject object: Any) -> String? {
+  /// Return a JSON encoded sstring representation of the given codable.
+  public static func jsonString(for object: JSONCodable) -> String? {
     do {
       var options: JSONSerialization.WritingOptions = []
       if #available(iOS 11.0, OSX 10.13, *) {
