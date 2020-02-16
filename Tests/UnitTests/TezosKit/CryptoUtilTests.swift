c544431278b614b479dac226d9594c35b093b296
keefertaylor <keefer.t@gmail.com>
2020-02-16 15:39:31 -0500

    add some tests for bad inputs

--- Tests/UnitTests/TezosKit/CryptoUtilTests.swift
+++ Tests/UnitTests/TezosKit/CryptoUtilTests.swift
@@ -74,4 +74,28 @@ class CryptoUtilsTest: XCTestCase {
 
     XCTAssertEqual(CryptoUtils.compressKey(uncompressedBytes), expectedCompressedBytes)
   }
+
+  public func testCompressKey_badLength() {
+    guard
+      let uncompressedBytes = CryptoUtils.hexToBin("00")
+    else {
+      XCTFail()
+      return
+    }
+
+    XCTAssertNil(CryptoUtils.compressKey(uncompressedBytes))
+  }
+
+  public func testCompressKey_badMagicByte() {
+    guard
+      let uncompressedBytes = CryptoUtils.hexToBin(
+        "0214fc03b8df87cd7b872996810db8458d61da8448e531569c8517b469a119d267be5645686309c6e6736dbd93940707cc9143d3cf29f1b877ff340e2cb2d259ce"
+      )
+    else {
+      XCTFail()
+      return
+    }
+
+    XCTAssertNil(CryptoUtils.compressKey(uncompressedBytes))
+  }
 }
