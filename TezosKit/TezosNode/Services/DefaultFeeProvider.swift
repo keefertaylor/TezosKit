5b0c4f65e37766c174620311fb529ba795010f8f
keefertaylor <keefer.t@gmail.com>
2020-05-02 17:05:27 +0900

    disallow negative Tez values

--- TezosKit/TezosNode/Services/DefaultFeeProvider.swift
+++ TezosKit/TezosNode/Services/DefaultFeeProvider.swift
@@ -77,13 +77,13 @@ public class DefaultFeeProvider {
         gasLimit: 10_307,
         storageLimit: 257
       )
-	// TODO(keefertaylor): Something more sane here
-	case .origination:
-	  return OperationFees(
-		fee: Tez(0.001_284),
-		gasLimit: 10_200,
-		storageLimit: 257
-	  )
+    // TODO(keefertaylor): Something more sane here
+    case .origination:
+      return OperationFees(
+        fee: Tez(0.001_284),
+        gasLimit: 10_200,
+        storageLimit: 257
+      )
     }
   }
 
@@ -108,7 +108,7 @@ public class DefaultFeeProvider {
         gasLimit: 10_307,
         storageLimit: 257
       )
-	// TODO(keefertaylor): Something more sane here
+    // TODO(keefertaylor): Something more sane here
     case .origination:
       return OperationFees(
         fee: Tez(0.001_284),
